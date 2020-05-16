97118eb232171a74de93a0b538330297181a0354
oiuv <i@oiuv.cn>
2020-05-16 22:13:35 +0800

    更新框架

--- include/globals.h
+++ include/globals.h
@@ -2,6 +2,9 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
 
+// 游戏管理员帐号
+#define WIZARD "test"
+
 // 功能模块
 #include "inherit.h"
 // 守护进程
@@ -9,12 +12,13 @@
 // 标准继承对象
 #include "stdob.h"
 
+// 指令路径
+#define CMD_PATH_WIZ ({"/cmds/wiz/", "/mudcore/cmds/wizard/"})
+#define CMD_PATH_STD ({"/cmds/std/", "/mudcore/cmds/player/"})
+
 // 其他
 #define MOTD    "/system/etc/motd"
 #define PRELOAD "/system/etc/preload"
-
-#define AUTO_LOOK
-#define WIZARD "test"
 
 // mudcore框架头文件（请放在最底部）
 #include <mudcore.h>
