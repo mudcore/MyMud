779c3e18f171aef152f85e5bc892f62c9fc1b8ed
oiuv <i@oiuv.cn>
2020-05-16 21:55:55 +0800

    根据框架升级教程

--- include/globals.h
+++ include/globals.h
@@ -2,8 +2,12 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
 
-#define AUTO_LOOK
+// 游戏管理员帐号
 #define WIZARD "test"
+
+// 指令路径
+#define CMD_PATH_WIZ ({"/cmds/wiz/", "/mudcore/cmds/wizard/"})
+#define CMD_PATH_STD ({"/cmds/std/", "/mudcore/cmds/player/"})
 
 // mudcore框架头文件（请放在最底部）
 #include <mudcore.h>
