59d844b7b3b472939980971f20adcdcd47c7971d
Oiuv <i@oiuv.cn>
2023-09-23 16:30:45 +0800

    更新框架

--- run.bat
+++ run.bat
@@ -1,3 +1,8 @@
 @echo off
-start /B "" "driver.exe" "config.cfg"
-color 02
+if not exist "driver.exe" (
+    echo driver.exe not found, please compile with build_msys2.sh...
+    pause
+) else (
+    start /B "" "driver.exe" "config.cfg"
+    color 02
+)
